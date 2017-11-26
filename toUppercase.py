485fa6d7c1092612907e6721cff88621ec427a24
alexanderluong <alexander.luong@hotmail.com>
2017-11-26 00:28:58 -0500

    Added messages and feature to copy converted parameter to clipboard

--- toUppercase.py
+++ toUppercase.py
@@ -10,6 +10,12 @@ if len(sys.argv) > 2:
 if len(sys.argv) == 2:
 	inputString = sys.argv[1]
 	print(inputString.upper())
+	print('Would you like to copy this to the clipboard? (Type \'yes\' to copy)')
+	if input().lower() == 'yes':
+		pyperclip.copy(inputString.upper())
+		print('Copied to clipboard succesfully.')
 else:
+	print('Converting string from clipboard to uppercase.')
 	clipboardString = pyperclip.paste()
-	pyperclip.copy(clipboardString.upper()) 
+	pyperclip.copy(clipboardString.upper())
+	print('Conversion successful.') 
